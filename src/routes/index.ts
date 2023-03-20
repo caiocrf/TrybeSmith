1b64e581e063e92cb3514ed477aa6870954d125b
caio pinho <icandoit2026@gmail.com>
2023-03-20 20:21:33 -0300

    get in /orders

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productRoute from './productRoute';
 import userRoute from './userRoute';
+import orderRoute from './orderRoute';
 
 const route = express.Router();
 
 route.use(productRoute);
 route.use(userRoute);
+route.use(orderRoute);
 
 export default route;
