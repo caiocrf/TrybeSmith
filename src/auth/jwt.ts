a1867c1d8bdfb10f65c46137d8f2914a210045c3
caio pinho <icandoit2026@gmail.com>
2023-04-20 11:05:51 -0300

    auth jwt

--- src/auth/jwt.ts
+++ src/auth/jwt.ts
@@ -1,14 +1,13 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
-import { ILogin } from '../Interface/login';
 
 const secret = process.env.JWT_SECRET || 'secret';
 
 const jwtConfig: SignOptions = { 
   algorithm: 'HS256', 
-  expiresIn: '5d', 
+  expiresIn: '7d', 
 };
 
-export const generateToken = (payload: ILogin) =>
-  jwt.sign(payload, secret, jwtConfig);
+export const generateToken = (data:object) =>
+  jwt.sign({ data }, secret, jwtConfig);
 
 export default generateToken;
